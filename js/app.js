e038a4b46ee97609dfe16d6f26b0124020b49ab3
Leon Lonsdale <leonlonsdale@icloud.com>
2022-08-15 18:33:29 +0100

    constructor, read toggle prototype, add book func

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 'use strict';
+
+function Book(title, author, pages, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
+
+const books = [];
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  books.push(newBook);
+}
