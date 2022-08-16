b89d2d6b05b8f09d80e635b789396001a99efbcd
Leon Lonsdale <leonlonsdale@icloud.com>
2022-08-16 23:09:50 +0100

    variable name refactor

--- js/app.js
+++ js/app.js
@@ -2,12 +2,14 @@
 
 let currentView = true;
 const library = [];
-const READ = document.querySelector('.books__read');
-const btnOpenModal = document.getElementById('add-book');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnSubmit = document.getElementById('submit');
+const bookDisplayEle = document.querySelector('.books__read');
+
+const openModalBtn = document.getElementById('add-book');
+const closeModalBtn = document.querySelector('.btn--close-modal');
+const submitBtn = document.getElementById('submit');
+
+const modalEle = document.querySelector('.modal');
+const overlayEle = document.querySelector('.overlay');
 
 function Book(title, author, pages, isbn, read = false) {
   this.title = title;
@@ -36,64 +38,64 @@ const remBook = (index) => {
 };
 
 const clear = () => {
-  READ.innerHTML = '';
+  bookDisplayEle.innerHTML = '';
 };
 
 const updateDisplay = () => {
   clear();
   for (const [index, book] of library.entries()) {
     if (book.read === currentView) {
-      const CARD = document.createElement('li');
-      CARD.classList.add('card');
-      const IMG = document.createElement('img');
+      const cardEle = document.createElement('li');
+      cardEle.classList.add('card');
+      const coverImgEle = document.createElement('img');
       if (book.isbn === 'No ISBN') {
-        IMG.setAttribute('src', 'assets/img/nc-md.gif');
-        IMG.setAttribute('alt', 'Missing book cover image');
+        coverImgEle.setAttribute('src', 'assets/img/nc-md.gif');
+        coverImgEle.setAttribute('alt', 'Missing book cover image');
       } else {
-        IMG.setAttribute(
+        coverImgEle.setAttribute(
           'src',
           `https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`
         );
-        IMG.setAttribute('alt', 'Book cover image');
+        coverImgEle.setAttribute('alt', 'Book cover image');
       }
-      IMG.classList.add('card__image');
+      coverImgEle.classList.add('card__image');
 
-      const BOOK_TITLE = document.createElement('h3');
-      BOOK_TITLE.classList.add(
+      const bookTitleLabel = document.createElement('h3');
+      bookTitleLabel.classList.add(
         'card__heading',
         'card__heading--main',
         'card--book-title'
       );
-      BOOK_TITLE.textContent = `${book.title}`;
-      const AUTHOR_TITLE = document.createElement('span');
-      AUTHOR_TITLE.classList.add('card__heading', 'card--author-title');
-      AUTHOR_TITLE.textContent = 'Author:';
-      const AUTHOR_NAME = document.createElement('span');
-      AUTHOR_NAME.classList.add('card__text', 'card--author-name');
-      AUTHOR_NAME.textContent = `${book.author}`;
-      const PAGES_TITLE = document.createElement('span');
-      PAGES_TITLE.classList.add('card__heading', 'card--pages-title');
-      PAGES_TITLE.textContent = 'Pages:';
-      const PAGES_COUNT = document.createElement('span');
-      PAGES_COUNT.classList.add('card__text', 'card--pages-count');
-      PAGES_COUNT.textContent = `${book.pages}`;
-      const READ_TITLE = document.createElement('span');
-      READ_TITLE.classList.add('card__heading', 'card--read-title');
-      READ_TITLE.textContent = 'Read?';
-      const TOGGLE = document.createElement('input');
-      TOGGLE.setAttribute('type', 'checkbox');
-      TOGGLE.classList.add('card--read-toggle');
+      bookTitleLabel.textContent = `${book.title}`;
+      const authorLabel = document.createElement('span');
+      authorLabel.classList.add('card__heading', 'card--author-title');
+      authorLabel.textContent = 'Author:';
+      const authorNameText = document.createElement('span');
+      authorNameText.classList.add('card__text', 'card--author-name');
+      authorNameText.textContent = `${book.author}`;
+      const pageCountLabel = document.createElement('span');
+      pageCountLabel.classList.add('card__heading', 'card--pages-title');
+      pageCountLabel.textContent = 'Pages:';
+      const pageCountText = document.createElement('span');
+      pageCountText.classList.add('card__text', 'card--pages-count');
+      pageCountText.textContent = `${book.pages}`;
+      const readStatusLabel = document.createElement('span');
+      readStatusLabel.classList.add('card__heading', 'card--read-title');
+      readStatusLabel.textContent = 'Read?';
+      const readSwitchEle = document.createElement('input');
+      readSwitchEle.setAttribute('type', 'checkbox');
+      readSwitchEle.classList.add('card--read-toggle');
 
-      CARD.append(IMG);
-      CARD.append(BOOK_TITLE);
-      CARD.append(AUTHOR_TITLE);
-      CARD.append(AUTHOR_NAME);
-      CARD.append(PAGES_TITLE);
-      CARD.append(PAGES_COUNT);
-      CARD.append(READ_TITLE);
-      CARD.append(TOGGLE);
+      cardEle.append(coverImgEle);
+      cardEle.append(bookTitleLabel);
+      cardEle.append(authorLabel);
+      cardEle.append(authorNameText);
+      cardEle.append(pageCountLabel);
+      cardEle.append(pageCountText);
+      cardEle.append(readStatusLabel);
+      cardEle.append(readSwitchEle);
 
-      READ.append(CARD);
+      bookDisplayEle.append(cardEle);
     }
   }
 };
@@ -108,32 +110,38 @@ const updateReadDisplay = (target) => {
 
 const openModal = (e) => {
   e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modalEle.classList.remove('hidden');
+  overlayEle.classList.remove('hidden');
 };
 
 const closeModal = (e) => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modalEle.classList.add('hidden');
+  overlayEle.classList.add('hidden');
 };
 
-btnOpenModal.addEventListener('click', openModal);
-overlay.addEventListener('click', closeModal);
-btnCloseModal.addEventListener('click', closeModal);
+openModalBtn.addEventListener('click', openModal);
+overlayEle.addEventListener('click', closeModal);
+closeModalBtn.addEventListener('click', closeModal);
 
-btnSubmit.addEventListener('click', (e) => {
+submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  const BOOK_TITLE = document.getElementById('book-title').value;
-  const AUTHOR_NAME = document.getElementById('author').value;
-  const PAGES_COUNT = document.getElementById('pages').value;
+  const bookTitleValue = document.getElementById('book-title').value;
+  const authorNameValue = document.getElementById('author').value;
+  const pageCountValue = document.getElementById('pages').value;
 
-  const ISBN =
+  const isbnValue =
     document.getElementById('isbn').value === null
       ? 'No ISBN'
       : document.getElementById('isbn').value;
 
-  const READ = true;
-  addBook(BOOK_TITLE, AUTHOR_NAME, PAGES_COUNT, ISBN, READ);
+  const readStatus = true;
+  addBook(
+    bookTitleValue,
+    authorNameValue,
+    pageCountValue,
+    isbnValue,
+    readStatus
+  );
 
   closeModal();
   document.getElementById('book-title').value = '';
