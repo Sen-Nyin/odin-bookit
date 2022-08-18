5b4c4259acdd0ae55dd2d14d1cc6530d594a3488
Leon Lonsdale <leonlonsdale@icloud.com>
2022-08-18 10:48:25 +0100

    refactor didnt push correctly

--- js/app.js
+++ js/app.js
@@ -3,19 +3,17 @@
 let currentView = 'all';
 const library = []; // [ book1, boo]
 const bookDisplayEle = document.querySelector('.bookshelf');
-
 const openModalBtn = document.getElementById('add-book');
 const closeModalBtn = document.querySelector('.btn--close-modal');
 const submitBtn = document.getElementById('submit');
 const readBtn = document.getElementById('readbtn');
 const unreadBtn = document.getElementById('unreadbtn');
 const allBtn = document.getElementById('allbtn');
-
 const modalEle = document.querySelector('.modal');
 const overlayEle = document.querySelector('.overlay');
-
 const readSwitches = document.querySelectorAll('[data-index]');
 
+// constructor management
 function Book(title, author, pages, isbn, read = false) {
   this.title = title;
   this.author = author;
@@ -23,34 +21,34 @@ function Book(title, author, pages, isbn, read = false) {
   this.isbn = isbn;
   this.read = read;
 }
-
 Book.prototype.toggleRead = function () {
   console.log(this);
   this.read = !this.read;
   console.log(this.title, ' toggled to ', this.read);
 };
 
+// book management
 const addBook = (title, author, pages, isbn, read) => {
   const newBook = new Book(title, author, pages, isbn, read);
   library.push(newBook);
   updateDisplay();
 };
-
 const removeBook = (index) => {
   library.splice(index, 1);
   updateDisplay();
 };
 
+// display management
 const clearDisplay = () => {
   bookDisplayEle.innerHTML = '';
 };
-
 const updateDisplay = () => {
   clearDisplay();
   for (const [index, book] of library.entries()) {
     if (book.read === currentView || currentView === 'all') {
       const cardEle = document.createElement('li');
       cardEle.classList.add('card', `${book.read ? 'read' : 'unread'}`);
+      // book cover
       const coverImgEle = document.createElement('img');
       if (!book.isbn) {
         coverImgEle.setAttribute('src', 'assets/img/nc-md.gif');
@@ -63,13 +61,13 @@ const updateDisplay = () => {
         coverImgEle.setAttribute('alt', 'Book cover image');
       }
       coverImgEle.classList.add('card__image');
-
+      // book title
       const bookTitleLabel = document.createElement('h3');
       bookTitleLabel.classList.add('card__heading', 'card--book-title');
       bookTitleLabel.textContent = `${
         !book.title ? 'Missing Title' : book.title
       }`;
-
+      // author display
       const authorLabel = document.createElement('span');
       authorLabel.classList.add('card--author-title');
       authorLabel.textContent = 'by ';
@@ -79,19 +77,18 @@ const updateDisplay = () => {
         !book.author ? 'Missing author' : book.author
       }`;
       authorLabel.append(authorNameText);
-
+      // page count display
       const pageCountLabel = document.createElement('span');
       pageCountLabel.classList.add('card--pages-title');
       pageCountLabel.textContent = 'pages';
-
       const pageCountText = document.createElement('span');
       pageCountText.classList.add('card--pages-count');
       pageCountText.textContent = `${!book.pages ? 'Unknown' : book.pages} `;
       pageCountLabel.prepend(pageCountText);
-
+      // controls
       const controlsContainer = document.createElement('div');
       controlsContainer.classList.add('card__controls');
-
+      //    read switch
       const readSwitchEle = document.createElement('label');
       readSwitchEle.classList.add('switch');
       const readSwitchLabel = document.createElement('span');
@@ -111,13 +108,12 @@ const updateDisplay = () => {
       const readSwitchSlider = document.createElement('span');
       readSwitchSlider.classList.add('switch__slider', 'round');
       readSwitchEle.append(readSwitchSlider);
-
+      //    delete book
       const deleteBtn = document.createElementNS(
         'http://www.w3.org/2000/svg',
         'svg'
       );
       deleteBtn.classList.add('btn--delete-book');
-
       const deleteIcon = document.createElementNS(
         'http://www.w3.org/2000/svg',
         'use'
@@ -128,53 +124,28 @@ const updateDisplay = () => {
       );
       deleteBtn.append(deleteIcon);
       deleteBtn.addEventListener('click', () => removeBook(index));
-
       controlsContainer.append(readSwitchEle);
       controlsContainer.append(deleteBtn);
-
       cardEle.append(coverImgEle);
       cardEle.append(bookTitleLabel);
       cardEle.append(authorLabel);
       cardEle.append(pageCountLabel);
       cardEle.append(controlsContainer);
-
       bookDisplayEle.append(cardEle);
     }
   }
 };
 
+// new book form management
 const openModal = (e) => {
   e.preventDefault();
   modalEle.classList.remove('hidden');
   overlayEle.classList.remove('hidden');
 };
-
 const closeModal = (e) => {
   modalEle.classList.add('hidden');
   overlayEle.classList.add('hidden');
 };
-// toggle between viewing all / read / unread books
-const toggleDisplay = (e) => {
-  const target = e.target.id;
-  if (target === 'readbtn') {
-    currentView = true;
-    allBtn.classList.remove('active');
-    readBtn.classList.add('active');
-    unreadBtn.classList.remove('active');
-  } else if (target === 'unreadbtn') {
-    currentView = false;
-    allBtn.classList.remove('active');
-    readBtn.classList.remove('active');
-    unreadBtn.classList.add('active');
-  } else {
-    currentView = 'all';
-    allBtn.classList.add('active');
-    readBtn.classList.remove('active');
-    unreadBtn.classList.remove('active');
-  }
-  updateDisplay();
-};
-
 const clearForm = () => {
   document.getElementById('book-title').value = '';
   document.getElementById('author').value = '';
@@ -200,6 +171,29 @@ const submitForm = (e) => {
   clearForm();
 };
 
+// page HUD
+const toggleDisplay = (e) => {
+  const target = e.target.id;
+  if (target === 'readbtn') {
+    currentView = true;
+    allBtn.classList.remove('active');
+    readBtn.classList.add('active');
+    unreadBtn.classList.remove('active');
+  } else if (target === 'unreadbtn') {
+    currentView = false;
+    allBtn.classList.remove('active');
+    readBtn.classList.remove('active');
+    unreadBtn.classList.add('active');
+  } else {
+    currentView = 'all';
+    allBtn.classList.add('active');
+    readBtn.classList.remove('active');
+    unreadBtn.classList.remove('active');
+  }
+  updateDisplay();
+};
+
+// events
 openModalBtn.addEventListener('click', openModal);
 overlayEle.addEventListener('click', closeModal);
 closeModalBtn.addEventListener('click', closeModal);
